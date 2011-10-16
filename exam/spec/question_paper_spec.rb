26377dd49a88e823b4156d1203569e5a0226166f
onisis20@hotmail.com <user.email>
2011-10-16 08:26:24 +0100

    check if the size of the array was zero , ok

--- exam/spec/question_paper_spec.rb
+++ exam/spec/question_paper_spec.rb
@@ -5,7 +5,8 @@ describe QuestionPaper do
   subject {QuestionPaper.new}
 
   it "should have an empty array" do
+    question = mock('Question')
     subject.questions.should be_an_instance_of(Array)
-   # subject.questions.size.should==0;
+    subject.questions.size.should==0;
   end
 end
