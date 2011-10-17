430ee7810cd6fe9cc528f1f1772684ba3ddc8470
onisis20@hotmail.com <user.email>
2011-10-17 15:19:00 +0100

    update doctor class

--- Group Project/lib/doctor.rb
+++ Group Project/lib/doctor.rb
@@ -2,12 +2,16 @@ require 'person.rb'
 
 class Doctor < Person
   attr_accessor :speciality
-  attr_accessor :user_name
-  attr_accessor :password
-  def initialize(user_name,password,id)
-      @user_name = user_name
-    @password =password
-    @id = id
+
+  def initialize
+
   end
+
+  def addDoctor(id, name, surname, address, phoneNumber, specialization, idNumber, password)
+    file = CSV.open("../csv/doctor.csv", "w")
+    file << [id, name, surname, address, phoneNumber, specialization, idNumber, password]
+  end
+
+
   # To change this template use File | Settings | File Templates.
 end
--- Group Project/lib/users.rb
+++ Group Project/lib/users.rb
@@ -1,9 +1,7 @@
 require 'csv'
 
 class Users
-  def file
 
-  end
 
   def addUser(id, name, surname, privileges)
     file = CSV.open("../csv/user.csv", "w")
