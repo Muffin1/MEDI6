9feb79c7b3bcdbebdcd990c3012111930653c8d4
mammad <mammad@gmail.com>
2011-10-20 16:28:21 +0100

    ID generator

--- Group Project/lib/person.rb
+++ Group Project/lib/person.rb
@@ -3,8 +3,27 @@ class Person
   attr_accessor :firstName , :lastName, :id, :userName, :password, :address, :phoneNumber
 
   def initialize()
-
   end
 
+  def idGenerator()
 
+    if File.exists?('../lib/id_file.txt')
+      file = File.open('../lib/id_file.txt','r')
+      file.each {|line|
+        @id = line.to_i
+      }
+      @id += 1
+      file.close
+
+      file = File.open('../lib/id_file.txt','w+')
+      file.write(@id)
+      file.close
+    else
+      file = File.open('../lib/id_file.txt','w+')
+      @id = 5000
+      file.write(@id)
+      file.close
+    end
+  end
 end
+
