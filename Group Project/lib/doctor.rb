ca515cbed914d8044e5cbc67e96dad8e979b9921
mammad <mammad@gmail.com>
2011-10-20 16:48:03 +0100

    Merged conflicts part 2

--- Group Project/lib/doctor.rb
+++ Group Project/lib/doctor.rb
@@ -2,7 +2,6 @@ require 'person.rb'
 
 
 class Doctor < Person
-<<<<<<< HEAD
 
 def addDoctor(doctorId,idNumber, firstName, lastName, address, phoneNumber, specialization,  password)
 
@@ -11,13 +10,11 @@ def addDoctor(doctorId,idNumber, firstName, lastName, address, phoneNumber, spec
     CSV::Writer.generate(file) do |csv|
     csv << [doctorId,idNumber, firstName, lastName, address, phoneNumber, specialization, password]
     end
-=======
   attr_accessor :speciality
 
   def initialize()
 
   end
->>>>>>> 3f164b2b7a61a03984c397856e7ba2c635f36852
 
   file.close
 end
