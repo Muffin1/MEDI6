62ed07d796b2dfefc695fea3923e5e13b63018a0
Sandra Alkiviadous <a_sandra_a@hotmail.com>
2011-10-20 03:33:33 -1200

    updated version of LogIn class

--- Group Project/lib/doctor.rb
+++ Group Project/lib/doctor.rb
@@ -2,13 +2,9 @@ require 'person.rb'
 
 class Doctor < Person
   attr_accessor :speciality
-<<<<<<< HEAD
 
   def initialize()
-=======
-  def initialize()
 
->>>>>>> 67eb16a729747ab37767d9e5c56e203330a864e3
   end
 
   def addDoctor(id, name, surname, address, phoneNumber, specialization, idNumber, password)
