e6802d7f926994c30d44e8ae6aeba5fd2016ac99
Sandra Alkiviadous <alkivias@uf089.cs.man.ac.uk>
2011-10-24 12:00:54 +0100

    refactoring initial tests for receptionist class

--- Group Project/spec/receptionist_spec.rb
+++ Group Project/spec/receptionist_spec.rb
@@ -10,8 +10,25 @@ describe "Receptionist" do
   it "Receptionist should have a first name" do
     receptionist.first_name = "Farhad"
     receptionist_fname = receptionist.first_name
-
     receptionist_fname.should == "Farhad"
   end
 
+  it "Receptionist should have a last name" do
+    receptionist.last_name = "Ghavam"
+    receptionist_last_name = receptionist.last_name
+    receptionist_last_name.should == "Ghavam"
   end
+
+  it "Receptionist should have an address" do
+    receptionist.address = "Manchester"
+    receptionist_address = receptionist.address
+    receptionist_address.should == "Manchester"
+  end
+
+  it "Receptionist should have a phone" do
+    receptionist.phone_number = "123456789"
+    phone_number = receptionist.phone_number
+    receptionist.phone_number.should == "123456789"
+  end
+
+end
