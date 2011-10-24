1946a6393636e4a6ff341d85ef0388148292d2ac
Sandra Alkiviadous <alkivias@uf089.cs.man.ac.uk>
2011-10-24 11:16:18 +0100

    mock test for

--- Group Project/spec/admin_spec.rb
+++ Group Project/spec/admin_spec.rb
@@ -6,6 +6,18 @@ require "../lib/receptionist.rb"
 describe "Administrator" do
 
   admin = Admin.new
+  let(:receptionist){mock('Receptionist')}
+
+  context "the admin should be able to register new doctors or receptionist in the system" do
+    it "defining add_doctor method and assign privileges" do
+
+      id = doctor.stub!(:id_generator).and_return(1)
+      doctor.stub!(:add_doctor).with(id,2, "John", "James", "27 Cherry Street", "12345678910", "pathologist",  "34m43")
+      doctor.stub!(:set_privileges).with(id, "5000", "d")
+      doctor.add_doctor(id,2, "John", "James", "27 Cherry Street", "12345678910", "pathologist",  "34m43")
+      doctor.set_privileges(id, "5000", "d")
+    end
+  end
 
 
 end
--- Group Project/spec/doctor_spec.rb
+++ Group Project/spec/doctor_spec.rb
@@ -1,5 +1,5 @@
 require "rspec"
-require 'doctor.rb'
+require "../lib/doctor"
 
 describe "Doctor" do
 
@@ -12,10 +12,10 @@ describe "Doctor" do
     end
   end
 
-   describe "methods of class doctor" do
-     doctor = mock ("Doctor")
+  describe "methods of class doctor" do
+    doctor = mock ("Doctor")
 
-      it "should register a new doctor to the system" do
+    it "should register a new doctor to the system" do
       doctor.stub!(:add_doctor)
       doctor.add_doctor(1,2, "John", "James", "27 Cherry Street", "12345678910", "pathologist",  "34m43")
 
@@ -23,7 +23,7 @@ describe "Doctor" do
       File.stub!(:open).with("filename", "privilege").and_yield(file)
       File.stub!(:write).with(1,2, "John", "James", "27 Cherry Street", "12345678910", "pathologist",  "34m43").and_return("1,2,John,James,27Cherry Street,12345678910,pathologist,34m43")
       File.write(1,2, "John", "James", "27 Cherry Street", "12345678910", "pathologist",  "34m43") == "1,2,John,James,27Cherry Street,12345678910,pathologist,34m43"
-      end
+    end
   end
 
 end
