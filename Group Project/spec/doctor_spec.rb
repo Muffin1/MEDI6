12051f9902ea0560292b1b133c913fce7a263283
mammad <mammad@gmail.com>
2011-10-27 16:41:24 +0100

    modified doctor data

--- Group Project/spec/doctor_spec.rb
+++ Group Project/spec/doctor_spec.rb
@@ -1,5 +1,6 @@
 require "rspec"
 require "../lib/doctor.rb"
+require "csv"
 
 describe "Doctor" do
 
@@ -23,6 +24,27 @@ describe "Doctor" do
       File.stub!(:open).with("filename", "privilege").and_yield(file)
       File.stub!(:write).with(1,2, "John", "James", "27 Cherry Street", "dateofbirth", "12345678910", "doctor@emal.com", "pathologist",  "34m43").and_return("1,2,John,James,27Cherry Street,dateofbirth,12345678910,doctor@emal.com,pathologist,34m43")
       File.write(1,2, "John", "James", "27 Cherry Street", "dateofbirth", "12345678910", "doctor@emal.com", "pathologist",  "34m43") == "1,2,John,James,27Cherry Street,dateofbirth,12345678910,doctor@emal.com,pathologist,34m43"
+    end
+  end
+
+  describe "methods of class doctor" do
+
+    it "should update doctor data" do
+      doctor1 = Doctor.new
+      doctor1.id_number = "t070942"
+      doctor1.first_name = "Farhad"
+      doctor1.last_name=  "Ghavam"
+      doctor1.address = "Oxford Str"
+      doctor1.date_of_birth = "18/10/1980"
+      doctor1.phone_number= "6984573957"
+      doctor1.email = "farhad@yahoo.com"
+      doctor1.specialization = "Endocrinologist"
+      doctor1.password="123456"
+
+    new_data = [doctor1.id_number, doctor1.first_name, doctor1.last_name,doctor1.address,doctor1.date_of_birth, doctor1.phone_number,doctor1.email,doctor1.specialization,doctor1.password]
+
+      doctor.update_doctor_data(doctor1).should ==new_data
+
     end
   end
 
