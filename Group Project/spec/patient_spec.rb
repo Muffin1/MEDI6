0e8e6f1ce0b706398e2af193c7712126510d457f
onisis20@hotmail.com <user.email>
2011-11-02 23:12:31 +0000

    updating patient class tests

--- Group Project/spec/patient_spec.rb
+++ Group Project/spec/patient_spec.rb
@@ -9,6 +9,15 @@ describe "Patient" do
     patient_system_id = patient.id_generator()
     patient.add_patient(patient_system_id.to_s,"nil", "Kwstas", "Prekas", "Ellas 98", "10/10/1950", "234567788", "KPrekas@gmail.com", "3242434223")
 
+    csv_contents = CSV.read("../csv/patient.csv")
+
+    found_it=nil
+    csv_contents.each do |row|
+      if(row[0] == patient_system_id.to_s)
+        found_it= row
+      end
+    end
+    found_it.should ==[patient_system_id.to_s,"nil", "Kwstas", "Prekas", "Ellas 98", "10/10/1950", "234567788", "KPrekas@gmail.com", "3242434223"]
   end
 
 end
